14ab9b573cf4967a49b5de3098b8a50b739068ff
AlekseiBerezkin <aleksei.berezkin@gmail.com>
2021-05-21 20:34:02 +0300

    Added nuget and test webhook

---
+++ ServiceMaxTon/Controllers/BotController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using ServiceMaxTon.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ServiceMaxTon.Controllers
+{
+    [ApiController]
+    [Route("api/bot")]
+    public class BotController:ControllerBase
+    {
+
+        public async Task <IActionResult> Post([FromBody] Update update)
+        {
+            TelegramBotClient client = new TelegramBotClient(Settings.Token);
+
+            if(update.Type== Telegram.Bot.Types.Enums.UpdateType.Message)
+            {
+                await client.SendTextMessageAsync(update.Message.From.Id, "answer");
+            }
+
+            return Ok();
+
+        }
+    }
+}
---
+++ ServiceMaxTon/Model/Settings.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ServiceMaxTon.Model
+{
+     static public class Settings
+    {
+        public static string Token { get; } = "1787244610:AAHjA_pa6Z3CdGCxUtNkad-SYCvD4EVGzCo";
+        public static string Url { get; } = "";
+    }
+}
