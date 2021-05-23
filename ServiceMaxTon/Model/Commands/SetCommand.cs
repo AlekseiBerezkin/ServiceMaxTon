7ec0629b7c233e282a8ecfcb8a79bf5eddcdab40
AlekseiBerezkin <aleksei.berezkin@gmail.com>
2021-05-23 22:20:13 +0300

    Added class with commands

---
+++ ServiceMaxTon/Model/Commands/SetCommand.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ServiceMaxTon.Model.Commands
+{
+    public class SetCommand : Command
+    {
+        public override string Name => "Записать";
+
+        public override async void Execute(Message message, TelegramBotClient client)
+        {
+            long chatId = message.Chat.Id;
+            //Записать 5 1.5 2500 UV
+            await client.SendTextMessageAsync(chatId, Settings.workingDirectory);
+        }
+    }
+}
---
+++ ServiceMaxTon/Model/Commands/SetExpensesCommand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Telegram.Bot;
+using Telegram.Bot.Types;
+
+namespace ServiceMaxTon.Model.Commands
+{
+    public class SetExpensesCommand : Command
+    {
+        public override string Name => "Расходы";
+
+        public override void Execute(Message message, TelegramBotClient client)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
